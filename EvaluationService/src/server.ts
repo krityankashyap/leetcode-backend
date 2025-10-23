fccdf61e42b164830d3e76049e3be754944523e9
Krityan Kashyap <krityankashyap@Krityans-MacBook-Air.local>
2025-10-23 15:30:33 +0530

    Added containers and test the python code

--- EvaluationService/src/server.ts
+++ EvaluationService/src/server.ts
@@ -40,11 +40,43 @@ app.listen(serverConfig.PORT, async () => {
     await pullAllImage();
     console.log("pulling image successfully");
 
-    const container= await createNewDockerContainer({
+    await testPythonCode();
+
+});
+
+   async function testPythonCode(){
+    const pythonCode= `print("Hello world")`;
+
+    // 1. take the python code and dump in a file and run the python file in the container
+
+    const runCommand= `echo '${pythonCode}' >  code.py && python3 code.py`;
+
+    const container= await createNewDockerContainer({  // this is going to create the container
         ImageName: PYTHON_IMAGE,
-        cmdExecutable: ['echo', 'hello.py'],
-        memoryLimit: 1024*1024*1024 // 2GB
+        cmdExecutable: ["/bin/sh", "-c", runCommand],       // (a) "/bin/sh -> command that we r going to use shell command inside the conatiner" , (b) -c -> we r going to use command as string
+        memoryLimit: 1024*1024*1024, 
     });
 
-    await container?.start()
-});
+    console.log("Container created successfully", container?.id);
+
+    // in order to start the container
+    await container?.start();
+
+    /* Now once the container is created we have to do two things
+       1) We should be able to get the logs from the containers and parse them somehow
+       2) once the task has been done then we should remove the conatiner   
+    */
+       
+       const status= await container?.wait();  //Block until a container stops, then returns the exit code.
+       console.log("container status", status);
+
+       const logs= await container?.logs({
+        stdout: true,
+        stderr: true
+       });
+       
+       console.log("container logs is:" , logs?.toString());
+
+       await container?.remove();
+   }
+
